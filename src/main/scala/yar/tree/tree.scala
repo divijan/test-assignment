31bae685668fb7028565e3566b8345a0e6501803
divijan <yar.ilich@gmail.com>
2018-12-31 02:25:01 +0200

    Finished tree balancing Split implicit class LeafList to upper level class and implicit conversion in package object Added a ++ method to LeafList

--- src/main/scala/yar/tree/tree.scala
+++ src/main/scala/yar/tree/tree.scala
@@ -7,14 +7,34 @@ import scala.collection.mutable
 case class Node(children: List[Node], leaves: List[Leaf]) {
   lazy val totalWeight = leaves.map(_.weight).sum
 
-  def balance(w: Int): Node = { //w for upper limit for the total weight of leaves
-    
+  /**
+    * This method sorts leaves in all the tree and carries leaves
+    * that exceed the total weight limit for the node over to the next nodes
+    * in a depth-first order. After the tree is traversed, exceeding leaves are discarded
+    * @param w upper limit for the total weight of leaves
+    * @return balanced tree stemming from this Node
+    */
+  def balance(w: Int): Node = {
+    def balanceNode(carryOver: LeafList, node: Node): (Node, LeafList) = {
+      val (underLimit, thisCarryOver) = node.leaves.splitByTotal(w)
+      val totalCarryOver = carryOver ++ thisCarryOver
+
+      val (balancedChildren, childrenCarryOver) = balanceList(carryOver ++ thisCarryOver, node.children)
+      Node(balancedChildren, underLimit.list) -> childrenCarryOver
+    }
+
+    def balanceList(carryOver: LeafList, nodes: List[Node]): (List[Node], LeafList) = nodes match {
+      case h :: t => {
+        val (bHead, headCarryOver) = balanceNode(carryOver, h)
+        val (bTail, tailCarryOver) = balanceList(headCarryOver, t)
+        (bHead :: bTail) -> tailCarryOver
+      }
+      case Nil => Nil -> carryOver
+    }
+
+    balanceNode(List.empty[Leaf], this)._1
   }
 
-  private def balanceThis(carryOver: List[Leaf], weightLimit: Int): Node {
-
-
-  }
 }
 
 case class Leaf(weight: Int)
